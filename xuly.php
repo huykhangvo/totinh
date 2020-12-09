1e1504d8ef14b2dca4509807a3c6fe5b182172ce
Võ Huy Khang <47880923+huykhangvo@users.noreply.github.com>
2020-12-09 11:37:35 +0700

    Update xuly.php

--- xuly.php
+++ xuly.php
@@ -4,7 +4,7 @@ include('./db/ketnoi.php');
 <?php
     if (isset($_POST['gui'])) {
         $message = $_POST['message'];
-        $sql_insert = mysqli_query($con, "INSERT INTO tbl_totinh(tinnhan) value('$message')");
+        $sql_insert = mysqli_query($con, "INSERT INTO sql9381070(tinnhan) value('$message')");
 	}
 		header('Location: index.php');
 ?>
